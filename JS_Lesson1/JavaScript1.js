0ef4c0b354d9b88a5b1dc44fe906e6547a082e62
Arman KZ <armansauyenov@gmail.com>
2018-10-19 16:27:32 +0600

    Add project files.

---
+++ JS_Lesson1/JavaScript1.js
@@ -0,0 +1,42 @@
+﻿var Human = {
+    name: '',
+    age: '',
+    gender: '',
+
+    setName: function (name) {
+        this.name = name;
+    },
+    getName: function () {
+        return this.name;
+    },
+
+    setAge: function (age) {
+        this.age = age;
+    },
+    getAge: function () {
+        return this.age;
+    },
+
+    setGender: function (gender) {
+        this.gender = gender;
+    },
+    getGender: function () {
+        return this.gender;
+    },
+
+    getInfo: function () {
+        alert("Name: " + this.name);
+        alert("Age: " + this.age);
+        alert("Gender: " + this.gender);
+    }
+}
+
+Human.name = prompt("Enter your name: ");
+Human.age = prompt("Enter your age: ");
+Human.gender = prompt("Enter your gender: ");
+
+document.write(Human.getName() + " ");
+document.write(Human.getAge() + " ");
+document.write(Human.getGender() + " ");
+
+Human.getInfo();
